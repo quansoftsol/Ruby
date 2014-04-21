adc6f440bc54462d53344682afe2939238640f31
Quantitative Software Solutions <quantitativesoftwaresolutions@comcast.net>
2014-04-21 23:56:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomster::Application.config.secret_key_base = '359c45fd53d3fd52fbc64c0ad81a9044fd756e54100e50a37c60ece3eb76ed36717e87b3fbb07be9d8431def6d7059b05eb714bea17713535c6d83af0e3db9d2'
